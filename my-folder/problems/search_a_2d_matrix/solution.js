e9faf79deb3a1919ed4bdef0ceedfcbeea93255a
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-07 19:21:04 +0000

    Sync LeetCode submission - Search a 2D Matrix (javascript)

---
+++ my-folder/problems/search_a_2d_matrix/solution.js
@@ -0,0 +1,15 @@
+// binary search solution from love babbar video
+var searchMatrix = function(matrix, target) {
+    let start = 0, end = (matrix.length * matrix[0].length) - 1
+    
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        let midNum = 
+            matrix[Math.floor(mid / matrix[0].length)][mid % matrix[0].length]
+        
+        if(midNum === target) return true    
+        else if(midNum < target) start = mid + 1
+        else end = mid - 1
+    }
+    return false
+};
