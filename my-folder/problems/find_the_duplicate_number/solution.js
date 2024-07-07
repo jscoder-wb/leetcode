27dd07250df71ba8d25caf63dd9d64f3c5d2ec54
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-07 19:14:43 +0000

    Sync LeetCode submission - Find the Duplicate Number (javascript)

---
+++ my-folder/problems/find_the_duplicate_number/solution.js
@@ -0,0 +1,16 @@
+// floyds cycle detection (see neetcode video for clarification)
+var findDuplicate = function(nums) {
+    let slow = nums[0];
+    let fast = nums[nums[0]];
+    
+    while (slow != fast) { 
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    let slow2 = 0;
+    while (slow != slow2) {
+        slow = nums[slow];
+        slow2 = nums[slow2];
+    }
+    return slow;
+};
