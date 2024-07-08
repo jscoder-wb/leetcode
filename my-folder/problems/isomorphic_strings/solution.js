3815cdafcafc5f0720d6bc8dd2dc6081c0d37947
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 20:15:49 +0000

    Sync LeetCode submission - Isomorphic Strings (javascript)

---
+++ my-folder/problems/isomorphic_strings/solution.js
@@ -0,0 +1,13 @@
+var isIsomorphic = function (s, t) {
+  let mapST = {},
+    mapTS = {}
+  for (let i = 0; i < s.length; i++) {
+    let c1 = s[i],
+      c2 = t[i]
+    if ((c1 in mapST && mapST[c1] != c2) || (c2 in mapTS && mapTS[c2] != c1))
+      return false
+    mapST[c1] = c2
+    mapTS[c2] = c1
+  }
+  return true
+}
