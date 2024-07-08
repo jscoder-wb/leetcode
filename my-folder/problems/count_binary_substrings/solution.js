505bdb2f0273ca9bc14b8a83f31db60ac398d8c7
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 07:51:57 +0000

    Sync LeetCode submission - Count Binary Substrings (javascript)

---
+++ my-folder/problems/count_binary_substrings/solution.js
@@ -0,0 +1,8 @@
+var countBinarySubstrings = function(s) {
+    let curr = 1, prev = 0, ans = 0 // curr/prev signify number of zeros or ones
+    for (let i = 1; i < s.length; i++)
+        if (s[i] === s[i-1]) curr++
+        else ans += Math.min(curr, prev), prev = curr, curr = 1
+    return ans + Math.min(curr, prev)
+};
+// watch youtube to understand its easy
