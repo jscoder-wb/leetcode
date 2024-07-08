fa06dcc28d7bd5cbbaaa23140142454b6ba3616a
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 07:49:16 +0000

    Sync LeetCode submission - Longest Palindromic Substring (javascript)

---
+++ my-folder/problems/longest_palindromic_substring/solution.js
@@ -0,0 +1,16 @@
+var longestPalindrome = function(s) {
+  let longest = ""
+  const helper = function(i, j) {
+    while (i >= 0 && j < s.length && s[i] === s[j]) {
+      i--; j++
+    }
+    return s.slice(i + 1, j)
+  }
+  for (let i = 0; i < s.length; i++) {
+    let odd = helper(i, i)
+    let even = helper(i, i + 1)
+    let longerPalindrome = odd.length > even.length ? odd : even
+    if (longerPalindrome.length > longest.length) longest = longerPalindrome
+  }  
+  return longest
+};
