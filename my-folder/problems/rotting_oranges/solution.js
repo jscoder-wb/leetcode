b1a9096cd0f31935750b6ed3e561097cecccbfe6
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 17:24:48 +0000

    Sync LeetCode submission - Rotting Oranges (javascript)

---
+++ my-folder/problems/rotting_oranges/solution.js
@@ -0,0 +1,39 @@
+var orangesRotting = function (grid) {
+  const height = grid.length, width = grid[0].length, queue = []
+  let fresh = 0;
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (grid[i][j] === 2) queue.push([i, j]);
+      if (grid[i][j] === 1) fresh++;
+    }
+  }
+  let minute = 0;
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const [x, y] = queue.shift();
+      if (x - 1 >= 0 && grid[x - 1][y] === 1) {
+        grid[x - 1][y] = 2;
+        fresh--;
+        queue.push([x - 1, y]);
+      }
+      if (x + 1 < height && grid[x + 1][y] === 1) {
+        grid[x + 1][y] = 2;
+        fresh--;
+        queue.push([x + 1, y]);
+      }
+      if (y - 1 >= 0 && grid[x][y - 1] === 1) {
+        grid[x][y - 1] = 2;
+        fresh--;
+        queue.push([x, y - 1]);
+      }
+      if (y + 1 < width && grid[x][y + 1] === 1) {
+        grid[x][y + 1] = 2;
+        fresh--;
+        queue.push([x, y + 1]);
+      }
+    }
+    if (queue.length) minute++;
+  }
+  return fresh === 0 ? minute : -1;
+};
