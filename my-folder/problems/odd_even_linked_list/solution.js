b7314c4544bc2a5edcecc506da464458b0647bb5
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 12:00:01 +0000

    Sync LeetCode submission - Odd Even Linked List (javascript)

---
+++ my-folder/problems/odd_even_linked_list/solution.js
@@ -0,0 +1,12 @@
+var oddEvenList = function(head) {
+  if (!head || !head.next) return head;
+  let odd = head, even = head.next, firstEven = even;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = firstEven;
+  return head;
+};
