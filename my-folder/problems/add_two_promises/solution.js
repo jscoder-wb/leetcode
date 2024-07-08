f562adfe12f96de430e88437583e047b3a809874
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 17:30:54 +0000

    Sync LeetCode submission - Add Two Promises (javascript)

---
+++ my-folder/problems/add_two_promises/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+  // Wait for both promises to resolve
+  const [value1, value2] = await Promise.all([promise1, promise2]);
+  return value1 + value2;
+};
+
+// // Example usage:
+// var promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20));
+// var promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60));
+
+// addTwoPromises(promise1, promise2)
+//   .then(console.log); // Output: 7
