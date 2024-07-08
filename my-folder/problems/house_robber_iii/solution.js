64405d6bfa83eb6d843969a51dca29cde27eaa46
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 16:20:27 +0000

    Sync LeetCode submission - House Robber III (javascript)

---
+++ my-folder/problems/house_robber_iii/solution.js
@@ -0,0 +1,13 @@
+var rob = function (root) {
+  return helper(root)[0]
+}
+
+function helper(root) {
+  if (root == null) return [0, 0]
+  const left = helper(root.left)
+  const right = helper(root.right)
+  const currMax = Math.max(left[0] + right[0],
+                  root.val + left[1] + right[1])
+  const childrenMax = left[0] + right[0]
+  return [currMax, childrenMax]
+}
