0cce5050e67c743f5e0dc3285e905bff20139a35
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-08 20:42:20 +0000

    Sync LeetCode submission - Max Consecutive Ones (javascript)

---
+++ my-folder/problems/max_consecutive_ones/solution.js
@@ -0,0 +1,13 @@
+var findMaxConsecutiveOnes = function(nums) {
+  let count = 0, arr = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) count++; 
+    else {
+      arr.push(count);
+      count = 0;
+    }
+  }
+  arr.push(count);
+  return Math.max(...arr);
+};
