33084341142cac707c621a63df6e93ef74cfe714
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 07:00:07 +0000

    Sync LeetCode submission - Sum of All Subset XOR Totals (javascript)

---
+++ my-folder/problems/sum_of_all_subset_xor_totals/solution.js
@@ -0,0 +1,14 @@
+var subsetXORSum = function (nums) {
+  return findSum(nums, 0, 0);
+};
+
+function findSum(nums, curInd, curNum) {
+  if (curInd === nums.length) {
+    return curNum;
+  }
+
+  let includeInXor = findSum(nums, curInd + 1, curNum ^ nums[curInd]);
+  let notIncludeInXor = findSum(nums, curInd + 1, curNum);
+
+  return includeInXor + notIncludeInXor;
+}
