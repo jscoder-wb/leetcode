7edd7eb6bdb6de8dc7c312fc7f4f7b216d3ae92d
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 08:56:26 +0000

    Sync LeetCode submission - Hand of Straights (javascript)

---
+++ my-folder/problems/hand_of_straights/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} hand
+ * @param {number} groupSize
+ * @return {boolean}
+ */
+var isNStraightHand = function (nums, k) {
+  if (nums.length % k) return false
+  let map = {}, set = new Set(nums)
+  nums.forEach(x => map[x] ? map[x]++ : map[x] = 1)
+  let count = nums.length / k
+  while (count--) {
+    let min = Math.min(...set)
+    for (let i = min; i < min + k; i++) {
+      if (!map[i]) return false
+      if (--map[i] == 0) set.delete(i)
+    }
+  }
+  return true
+};
