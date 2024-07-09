c3d9069a068918716d027ba5d7382f7b1f83391f
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 07:08:58 +0000

    Sync LeetCode submission - Container With Most Water (javascript)

---
+++ my-folder/problems/container_with_most_water/solution.js
@@ -0,0 +1,9 @@
+// 2 pointer solution 
+var maxArea = function (H) {
+  let ans = 0, i = 0, j = H.length - 1
+  while (i < j) {
+    ans = Math.max(ans, Math.min(H[i], H[j]) * (j - i))
+    H[i] <= H[j] ? i++ : j--
+  }
+  return ans
+};
