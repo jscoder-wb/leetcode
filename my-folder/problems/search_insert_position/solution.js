2d615613691a4c9df084b319037a902f65e39c43
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 06:52:06 +0000

    Sync LeetCode submission - Search Insert Position (javascript)

---
+++ my-folder/problems/search_insert_position/solution.js
@@ -0,0 +1,15 @@
+// binary search solution
+var searchInsert = function (nums, target) {
+  let high = nums.length - 1;
+  let low = 0;
+  while (low <= high) {
+    let mid = Math.floor((high + low) / 2);
+    if (nums[mid] == target) return mid;
+    else if (nums[mid] > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
+};
