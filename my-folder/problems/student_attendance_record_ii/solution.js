1af8c11a6ed22aa6f49a576ff4845043b037d434
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 07:16:24 +0000

    Sync LeetCode submission - Student Attendance Record II (javascript)

---
+++ my-folder/problems/student_attendance_record_ii/solution.js
@@ -0,0 +1,24 @@
+// 3d dp memo solution
+var checkRecord = function (n) {
+  const MOD = 1000000007;
+
+  let dp = new Array(n).fill(0).map(() => new Array(2).fill(0).map(() => new Array(3).fill(-1)));
+  const check_all_records = (cur_ind, count_a, count_l) => {
+    if (cur_ind === n) return 1
+
+    if (dp[cur_ind][count_a][count_l] !== -1)
+      return dp[cur_ind][count_a][count_l]
+
+    let with_a_next = (count_a === 0)
+      ? check_all_records(cur_ind + 1, count_a + 1, 0) : 0;
+
+    let with_l_next = (count_l === 2)
+      ? 0 : check_all_records(cur_ind + 1, count_a, count_l + 1);
+
+    let with_p_next = check_all_records(cur_ind + 1, count_a, 0);
+    let total = ((with_a_next + with_l_next) % MOD + with_p_next) % MOD;
+
+    return dp[cur_ind][count_a][count_l] = total
+  }
+  return check_all_records(0, 0, 0);
+};
