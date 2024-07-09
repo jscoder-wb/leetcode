dc6c074e9fad0272daf04a51108df2c24e9f4ac7
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 07:33:06 +0000

    Sync LeetCode submission - Append Characters to String to Make Subsequence (javascript)

---
+++ my-folder/problems/append_characters_to_string_to_make_subsequence/solution.js
@@ -0,0 +1,11 @@
+// 2 pointer solution
+var appendCharacters = function(s, t) {
+    let i = 0, j = 0;
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) {
+            j++;
+        }
+        i++;
+    }
+    return t.length - j;
+};
