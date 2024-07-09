92ab48a1feb4eb4daad06e9f3df253b1f17d9c2a
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-09 06:55:20 +0000

    Sync LeetCode submission - Evaluate Boolean Binary Tree (javascript)

---
+++ my-folder/problems/evaluate_boolean_binary_tree/solution.js
@@ -0,0 +1,16 @@
+// Depth First Search 
+var evaluateTree = function (root) {
+  const dfs = (node) => {
+    if (!node) return;
+    if (node.val === 0) return false;
+    if (node.val === 1) return true;
+
+    if (node.val === 2) {
+      return dfs(node.left) || dfs(node.right);
+    } else if (node.val === 3) {
+      return dfs(node.left) && dfs(node.right);
+    }
+  };
+
+  return dfs(root);
+};
