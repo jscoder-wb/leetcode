9a818ee8ae046c31119ec5b83ee29934c2612292
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-10 00:07:12 +0000

    Sync LeetCode submission - Crawler Log Folder (javascript)

---
+++ my-folder/problems/crawler_log_folder/solution.js
@@ -0,0 +1,12 @@
+var minOperations = function (logs) {
+  let step = 0;
+  for (let i = 0; i < logs.length; i++) {
+    const log = logs[i];
+    if (log === '../') {
+      if (step > 0) step--;
+    } else if (log !== './') {
+      step++;
+    }
+  }
+  return step;
+};
