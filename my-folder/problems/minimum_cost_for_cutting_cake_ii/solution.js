8341f54baf9aadf0353ac9e499b1a97c7b9e2609
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-14 07:07:13 +0000

    Sync LeetCode submission - Minimum Cost for Cutting Cake II (javascript)

---
+++ my-folder/problems/minimum_cost_for_cutting_cake_ii/solution.js
@@ -0,0 +1,34 @@
+// greedy solution from codestorywithmik (same as I)
+var minimumCost = function (m, n, horizontalCut, verticalCut) {
+  const x = horizontalCut.length;
+  const y = verticalCut.length;
+
+  horizontalCut.sort((a, b) => a - b); 
+  verticalCut.sort((a, b) => a - b);  
+
+  let i = x - 1, j = y - 1; // Start from the largest elements
+  let horizontalPieces = 1, verticalPieces = 1, result = 0;
+
+  while (i >= 0 && j >= 0) {
+    if (horizontalCut[i] >= verticalCut[j]) {
+      result += horizontalCut[i] * verticalPieces;
+      horizontalPieces++;
+      i--;
+    } else {
+      result += verticalCut[j] * horizontalPieces;
+      verticalPieces++;
+      j--;
+    }
+  }
+  while (i >= 0) {
+    result += horizontalCut[i] * verticalPieces;
+    horizontalPieces++;
+    i--;
+  }
+  while (j >= 0) {
+    result += verticalCut[j] * horizontalPieces;
+    verticalPieces++;
+    j--;
+  }
+  return result;
+};
