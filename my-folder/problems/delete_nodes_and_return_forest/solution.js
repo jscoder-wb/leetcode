2325e65513ecdd8e5342cece26bc977213df7278
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-19 09:01:36 +0000

    Sync LeetCode submission - Delete Nodes And Return Forest (javascript)

--- my-folder/problems/delete_nodes_and_return_forest/solution.js
+++ my-folder/problems/delete_nodes_and_return_forest/solution.js
@@ -4,6 +4,7 @@ var delNodes = function (root, to_delete) {
     if (!root) return null;
     let shouldDelete = to_delete.includes(root.val);
     if (isRoot && !shouldDelete) roots.push(root);
+    // left and right will be ROOTS if current root was deleted
     root.left = helper(root.left, shouldDelete);
     root.right = helper(root.right, shouldDelete);
     return shouldDelete ? null : root;
