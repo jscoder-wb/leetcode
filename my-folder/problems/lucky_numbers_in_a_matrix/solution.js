cef44f110b374862dd3c7a725400f6d6cfd3faa5
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-19 09:28:16 +0000

    Sync LeetCode submission - Lucky Numbers in a Matrix (javascript)

---
+++ my-folder/problems/lucky_numbers_in_a_matrix/solution.js
@@ -0,0 +1,14 @@
+var luckyNumbers = function (matrix) {
+  const minInRows = new Set(), result = [];
+  for (const row of matrix) {
+    const minInRow = Math.min(...row);
+    minInRows.add(minInRow);
+  }
+  for (let col = 0; col < matrix[0].length; ++col) {
+    let maxInCol = matrix[0][col];
+    for (let row = 1; row < matrix.length; ++row) 
+      maxInCol = Math.max(maxInCol, matrix[row][col]);
+    if (minInRows.has(maxInCol)) result.push(maxInCol);
+  }
+  return result;
+};
