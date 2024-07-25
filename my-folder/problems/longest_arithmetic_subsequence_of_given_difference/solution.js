f8bab0d44cc44de61130d77e9e73ccc6323388a2
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-25 09:29:41 +0000

    Sync LeetCode submission - Longest Arithmetic Subsequence of Given Difference (javascript)

---
+++ my-folder/problems/longest_arithmetic_subsequence_of_given_difference/solution.js
@@ -0,0 +1,12 @@
+var longestSubsequence = function(arr, difference) {
+  let dp = {}, max_length = 0;
+  for (let n of arr) {
+    if (n - difference in dp) {
+      dp[n] = dp[n - difference] + 1;
+    } else {
+      dp[n] = 1;
+    }
+    max_length = Math.max(max_length, dp[n]);
+  }
+  return max_length;
+};
