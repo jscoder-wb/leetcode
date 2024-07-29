4c58eb0638f75710b639e79a43c3302d538b2460
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-29 19:37:31 +0000

    Sync LeetCode submission - Design Graph With Shortest Path Calculator (javascript)

--- my-folder/problems/design_graph_with_shortest_path_calculator/solution.js
+++ my-folder/problems/design_graph_with_shortest_path_calculator/solution.js
@@ -1,52 +1,41 @@
-// Dijkstra algorithm
+// solution from codeStorywithMIk (floyd warshall)
 class Graph {
   constructor(n, edges) {
-    this.adj = []
-    this.n = n
-    for (let[from,to,wt] of edges) {
-      if (!this.adj[from])
-        this.adj[from] = []
-      this.adj[from].push([to, wt])
+    this.n = n;
+    this.adjMat = Array.from({
+      length: n
+    }, () => Array(n).fill(Number.MAX_SAFE_INTEGER));
+
+    for (let i = 0; i < n; i++) {
+      this.adjMat[i][i] = 0;
     }
-    // filling empty array for nodes which point to nothing
-    for (let i = 0; i < n; i++)
-      if (!this.adj[i])
-        this.adj[i] = []
-  }
-  addEdge(edge) {
-    let[from,to,wt] = edge
-    if (!this.adj[from])
-      this.adj[from] = []
-    this.adj[from].push([to, wt])
-  }
-  shortestPath(node1, node2) {
-    const visited = {}
-    const output = [] // output[i] = shortest dist from start node to <i>
-    const queue = [] // queue will store shortest dist from point a to point b
-
-    for (let i = 0; i < this.n; i++) {
-      if (i === node1) {
-        output[i] = 0
-        queue.push({from: node1, to: i, dist: 0})
-      } else output[i] = Infinity
+    for (const edge of edges) {
+      this.adjMat[edge[0]][edge[1]] = edge[2];
     }
-
-    while(queue.length) {
-      const {to: node} = queue.shift()
-      if (!visited[node]) {
-        visited[node] = true
-
-        for (const [neighbor, wt] of this.adj[node]) {
-          let newDist = output[node] + wt
-          if (newDist < output[neighbor]) {
-            output[neighbor] = newDist
-            queue.push({from: node1, to: neighbor, dist: newDist})
-          }
-        } 
-        queue.sort((a, b) => a.dist - b.dist)
+    for (let via = 0; via < n; via++) {
+      for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+          this.adjMat[i][j] = Math.min(this.adjMat[i][j],
+            this.adjMat[i][via] + this.adjMat[via][j]);
+        }
       }
     }
-    return output[node2] === Infinity ? -1 : output[node2]
+  }
+  addEdge(edge) {
+    if (this.adjMat[edge[0]][edge[1]] <= edge[2]) return
+    this.adjMat[edge[0]][edge[1]] = edge[2];
+
+    for (let i = 0; i < this.n; i++) {
+      for (let j = 0; j < this.n; j++) {
+        this.adjMat[i][j] = Math.min(this.adjMat[i][j],
+          this.adjMat[i][edge[0]] + edge[2] + this.adjMat[edge[1]][j]);
+      }
+    }
+  }
+  shortestPath(node1, node2) {
+    if (this.adjMat[node1][node2] === Number.MAX_SAFE_INTEGER) {
+      return -1;
+    }
+    return this.adjMat[node1][node2];
   }
 }
-// practice
