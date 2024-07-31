ebfef383e8597fb852a08219a7fe9a1a840ecb83
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-07-31 14:49:32 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (javascript)

--- my-folder/problems/shortest_path_in_binary_matrix/solution.js
+++ my-folder/problems/shortest_path_in_binary_matrix/solution.js
@@ -1,21 +1,30 @@
-// bjs solution
-var shortestPathBinaryMatrix = function (grid) {
-  const n = grid.length
+// bfs solution from codestory 
+function shortestPathBinaryMatrix(grid) {
+  const n = grid.length;
   if (grid[0][0] || grid[n - 1][n - 1]) return -1
-  const queue = []
-  const dist = 0
-  queue.push([0, 0, dist + 1])
-  const directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-  for (const [i, j, d] of queue) {
-    if (i === n - 1 && j === n - 1) return d
-    for (const [x, y] of directions) {
-      const newI = i + x
-      const newJ = j + y
-      if (0 <= newI && newI < n && 0 <= newJ && newJ < n && !grid[newI][newJ]) {
-        queue.push([newI, newJ, d + 1])
-        grid[newI][newJ] = 1
+
+  const directions = [[1, 1], [0, 1], [1, 0], [0, -1], [-1, 0], [-1, -1], [1, -1], [-1, 1]];
+
+  const Q = [[0, 0, 1]]; // i, j, distance
+  grid[0][0] = 1; // mark first cell as visited
+
+  while (Q.length) {
+    let N = Q.length;
+    while (N--) {
+      const [i, j, d] = Q.shift();
+
+      if (i === n - 1 && j === n - 1) return d
+
+      for (const [x, y] of directions) {
+        const newI = i + x, newJ = j + y
+
+        if (newI >= 0 && newI < n && newJ >= 0 && newJ < n
+         && grid[newI][newJ] === 0) {
+          Q.push([newI, newJ, d + 1]);
+          grid[newI][newJ] = 1; // marking visited
+        }
       }
-    }
+    } 
   }
-  return -1
-};
+  return -1;
+}
