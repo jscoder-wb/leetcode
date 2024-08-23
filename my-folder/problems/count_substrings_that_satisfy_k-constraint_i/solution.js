67205c2ae6a6e3590089cff320a665c8f8b2b328
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-08-23 10:39:48 +0000

    Sync LeetCode submission - Count Substrings That Satisfy K-Constraint I (javascript)

--- my-folder/problems/count_substrings_that_satisfy_k-constraint_i/solution.js
+++ my-folder/problems/count_substrings_that_satisfy_k-constraint_i/solution.js
@@ -1,25 +1,21 @@
 var countKConstraintSubstrings = function (s, k) {
   let allSubstrings = [];
-  for (let i = 0; i < s.length; i++) {
-    allSubstrings.push(s[i]);
-    for (let j = i + 1; j < s.length; j++) {
-      allSubstrings.push(
-        allSubstrings[allSubstrings.length - 1] + s[j]
-      );
-    }
-  }
+  for (let i = 0; i < s.length; i++)
+    for (let j = i + 1; j <= s.length; j++)
+      allSubstrings.push(s.substring(i, j));
   function check(str) {
     let map = {}
     for (let ch of str) {
       map[ch] ? map[ch]++ : map[ch] = 1
-      if (map["1"] > k && map["0"] > k) return false
+      if (map["1"] > k && map["0"] > k) // demorgan law
+        return false
     }
     return true
   }
   let count = 0
   for (let str of allSubstrings) {
-    if (check(str)) count++
+    if (check(str))
+      count++
   }
   return count
 };
-// brute force
