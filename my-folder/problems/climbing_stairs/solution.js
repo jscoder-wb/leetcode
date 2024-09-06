8ec98da56a33636c7aefad4e09640d4e4cb7c078
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-09-06 21:57:51 +0000

    Sync LeetCode submission - Climbing Stairs (javascript)

--- my-folder/problems/climbing_stairs/solution.js
+++ my-folder/problems/climbing_stairs/solution.js
@@ -1,6 +1,6 @@
-var climbStairs = function(n) {
+var climbStairs = function (n) {
   // its a fibonacci
-  let dp = [0, 1, 2]
-  for(let i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2]
+  let dp = [0, 1, 2] // dp[i] element indicates total ways to reach ith step
+  for (let i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2]
   return dp[n]
 };
