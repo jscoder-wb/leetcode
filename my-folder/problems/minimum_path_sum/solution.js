e55e215c57f213854c6aec2dbb50cc81a8100cad
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-09-24 03:49:42 +0000

    Sync LeetCode submission - Minimum Path Sum (javascript)

--- my-folder/problems/minimum_path_sum/solution.js
+++ my-folder/problems/minimum_path_sum/solution.js
@@ -1,17 +1,17 @@
-var minPathSum = function(grid) {
-  const n = grid.length;
-  const m = grid[0].length;
+// memo solution
+var minPathSum = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+  const dp = Array.from({ length: m }, () => Array(n).fill(-1));
+  // dp element = min path sum from cell i, j to last cell
 
-  for (let i = 1; i < n; i++) {
-    grid[i][0] += grid[i - 1][0];
+  function helper(i, j) {
+    if (i === m - 1 && j === n - 1) return dp[i][j] = grid[i][j];
+    if (dp[i][j] !== -1) return dp[i][j];
+
+    if (i === m - 1) return dp[i][j] = grid[i][j] + helper(i, j + 1);
+    else if (j === n - 1) return dp[i][j] = grid[i][j] + helper(i + 1, j);
+    else return dp[i][j] = grid[i][j] + Math.min(helper(i + 1, j), helper(i, j + 1));
   }
-  for (let j = 1; j < m; j++) {
-    grid[0][j] += grid[0][j - 1];
-  }
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < m; j++) {
-      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-    }
-  }
-  return grid[n - 1][m - 1];
+  return helper(0, 0);
 };
