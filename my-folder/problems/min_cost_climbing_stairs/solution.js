6ff2a5d896f266a86e06cea3d60f2040bd969597
jscoder-wb <152047155+jscoder-wb@users.noreply.github.com>
2024-10-26 10:51:55 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (javascript)

--- my-folder/problems/min_cost_climbing_stairs/solution.js
+++ my-folder/problems/min_cost_climbing_stairs/solution.js
@@ -1,11 +1,9 @@
-// bottom up dynamic programming
+// Question : You are given an integer array cost where cost[i] is the cost of ith step on a staircase. For each step 0 <= i < n we have to pay a cost of cost[i] to enter that step. Return the min cost to get to top of the floor ie nth step. We dont have to pay the cost of nth step.  
+
 var minCostClimbingStairs = function(cost) {
-    let n = cost.length; 
-    let dp = [cost[0], cost[1]] // ith val represents min cost to react that step 
+    let n = cost.length, dp = [cost[0], cost[1]] // dp[i] = min cost to get to ith step. 
+    for(let i = 2; i < n; i++) dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2])
     
-    for(let i = 2; i < n; i++) {
-     dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2])
-    }
-    // we can either pick the second last step to reach the top by steping 2 steps OR we can pick the last step to react the top by steping 1 step
-    return Math.min(dp[n-1], dp[n-2])
+    return Math.min(dp[n-1], dp[n-2]);
+    // we can either pick the second last step to reach the top by steping 2 steps OR we can pick the last step to reach the top by steping 1 step
 };
